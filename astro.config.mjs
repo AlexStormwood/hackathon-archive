c1f20092c5410e7e336b5bd09abcebf60030cc47
Alex <alex.holder@bigfootds.com>
2025-01-15 10:40:57 +1100

    fix: correct astro config for gh pages subsite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://alexstormwood.github.io',
+  	base: 'hackathon-archive',
 	integrations: [
 		starlight({
 			title: "Bigfoot's Hackathon Archive",
